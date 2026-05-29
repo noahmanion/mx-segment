3e9056e2672c1caec67bab8e20a520b13c3a4077
Segment Git Sync <friends@segment.com>
2026-05-29 18:01:42 +0000

    Destination '69fca00eef1fc24e59380a50' modified by noah@softpath.co

--- destinations/69fca00eef1fc24e59380a50.tf
+++ destinations/69fca00eef1fc24e59380a50.tf
@@ -53,7 +53,7 @@ resource "segment_destination" "id-69fca00eef1fc24e59380a50" {
     pipedriveOrgFieldPlan            = "a46adb4c76b9b5f2da6ec82bf1598f8e94892422"
     pipedrivePipelineId              = "8"
     pipedriveStageChurnDetected      = "45"
-    pipedriveStageLeadIncoming       = ""
+    pipedriveStageLeadIncoming       = "40"
     pipedriveStageOnboarded          = "42"
     pipedriveStagePaid               = "44"
     pipedriveStageQualification      = "40"
