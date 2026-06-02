8abd2b80d0c7a3778e4a020f5bbf065a920aa778
Segment Git Sync <friends@segment.com>
2026-06-02 16:09:26 +0000

    Destination Subscription '8iMCrWySvG4D5hcvvtMc7p' created by noah@softpath.co

---
+++ destination_subscriptions/6a1f00344db171f61bf445b2_8iMCrWySvG4D5hcvvtMc7p.tf
@@ -0,0 +1,21 @@
+import {
+  to = segment_destination_subscription.id-6a1f00344db171f61bf445b2_8iMCrWySvG4D5hcvvtMc7p
+  id = "6a1f00344db171f61bf445b2:8iMCrWySvG4D5hcvvtMc7p"
+}
+
+resource "segment_destination_subscription" "id-6a1f00344db171f61bf445b2_8iMCrWySvG4D5hcvvtMc7p" {
+  action_id            = "ja2fMtPLyGVf5gRvcPg2Km"
+  destination_id       = "6a1f00344db171f61bf445b2"
+  enabled              = true
+  model_id             = null
+  name                 = "Send Server"
+  reverse_etl_schedule = null
+  settings = jsonencode({
+    data = {
+      "@path" = "$."
+    }
+    method = "POST"
+    url    = "https://gambling-plenty-stallion.ngrok-free.dev/ingest?src=server"
+  })
+  trigger = "type = \"track\" and type = \"identify\""
+}
