b47a392109f3a8a922a18d29c3899ecc393b0d7f
Segment Git Sync <friends@segment.com>
2026-06-05 15:57:28 +0000

    Destination Subscription '65wNcAbF8YvvPn5NW3VpaF' modified by noah@softpath.co

--- destination_subscriptions/6a1f3f97a84c3cb88a8c575d_65wNcAbF8YvvPn5NW3VpaF.tf
+++ destination_subscriptions/6a1f3f97a84c3cb88a8c575d_65wNcAbF8YvvPn5NW3VpaF.tf
@@ -21,7 +21,7 @@ resource "segment_destination_subscription" "id-6a1f3f97a84c3cb88a8c575d_65wNcAb
     event_id = {
       "@path" = "$.messageId"
     }
-    event_name = "CompleteRegistration"
+    event_name = "AccountCreated"
     event_source_url = {
       "@path" = "$.context.page.url"
     }
