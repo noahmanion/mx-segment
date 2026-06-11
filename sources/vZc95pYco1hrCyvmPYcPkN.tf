6d5a1eb89196d871dffe0130f9d233decd81ec7f
Segment Git Sync <friends@segment.com>
2026-06-11 13:28:08 +0000

    Source 'vZc95pYco1hrCyvmPYcPkN' modified by noah@softpath.co

--- sources/vZc95pYco1hrCyvmPYcPkN.tf
+++ sources/vZc95pYco1hrCyvmPYcPkN.tf
@@ -9,7 +9,14 @@ resource "segment_source" "id-vZc95pYco1hrCyvmPYcPkN" {
   metadata = {
     id = "mazatB39IS"
   }
-  name     = "Facebook Ads"
-  settings = jsonencode({})
-  slug     = "facebook_ads"
+  name = "Facebook Ads"
+  settings = jsonencode({
+    avatar      = "https://platform-lookaside.fbsbx.com/platform/profilepic/?asid=10101095105611717&height=50&width=50&ext=1783776487&hash=AfsnvKF2WwUMr8sI9BICC_P1"
+    createdAt   = "2026-06-11T13:28:07.259Z"
+    createdBy   = "noah@softpath.co"
+    displayName = "Noah Manion"
+    id          = "10101095105611717"
+    name        = "Noah Manion"
+  })
+  slug = "facebook_ads"
 }
