95d8a8441e6ed92be6a136bac5aafc3664a4b8e6
Segment Git Sync <friends@segment.com>
2026-06-11 13:27:50 +0000

    Source 'vZc95pYco1hrCyvmPYcPkN' created by noah@softpath.co

---
+++ sources/vZc95pYco1hrCyvmPYcPkN.tf
@@ -0,0 +1,15 @@
+import {
+  to = segment_source.id-vZc95pYco1hrCyvmPYcPkN
+  id = "vZc95pYco1hrCyvmPYcPkN"
+}
+
+resource "segment_source" "id-vZc95pYco1hrCyvmPYcPkN" {
+  enabled = false
+  labels  = null
+  metadata = {
+    id = "mazatB39IS"
+  }
+  name     = "Facebook Ads"
+  settings = jsonencode({})
+  slug     = "facebook_ads"
+}
