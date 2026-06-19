b47b24ca66e9068ad90643b5947077263e610d7c
Segment Git Sync <friends@segment.com>
2026-06-19 18:55:43 +0000

    Destination Subscription '6vjhrG6AaVaS1EsSuq71mD' created by noah@softpath.co

---
+++ destination_subscriptions/6a3590aca2ebd389a77447cc_6vjhrG6AaVaS1EsSuq71mD.tf
@@ -0,0 +1,111 @@
+import {
+  to = segment_destination_subscription.id-6a3590aca2ebd389a77447cc_6vjhrG6AaVaS1EsSuq71mD
+  id = "6a3590aca2ebd389a77447cc:6vjhrG6AaVaS1EsSuq71mD"
+}
+
+resource "segment_destination_subscription" "id-6a3590aca2ebd389a77447cc_6vjhrG6AaVaS1EsSuq71mD" {
+  action_id            = "pAMZTGDc7yKTW5ZGecP8Df"
+  destination_id       = "6a3590aca2ebd389a77447cc"
+  enabled              = true
+  model_id             = null
+  name                 = "Complete Registration"
+  reverse_etl_schedule = null
+  settings = jsonencode({
+    __segment_internal_sync_mode = "add"
+    batch_size                   = 1500
+    conversion_action            = "7651099651"
+    conversion_timestamp = {
+      "@path" = "$.timestamp"
+    }
+    currency = {
+      "@path" = "$.properties.currency"
+    }
+    email_address = {
+      "@if" = {
+        else = {
+          "@path" = "$.context.traits.email"
+        }
+        exists = {
+          "@path" = "$.properties.email"
+        }
+        then = {
+          "@path" = "$.properties.email"
+        }
+      }
+    }
+    gclid = {
+      "@path" = "$.properties.gclid"
+    }
+    items = {
+      "@arrayPath" = [{
+        "@path" = "$.properties.products"
+        }, {
+        price = {
+          "@path" = "$.price"
+        }
+        product_id = {
+          "@path" = "$.product_id"
+        }
+        quantity = {
+          "@path" = "$.quantity"
+        }
+      }]
+    }
+    order_id = {
+      "@if" = {
+        else = {
+          "@path" = "$.properties.order_id"
+        }
+        exists = {
+          "@path" = "$.properties.orderId"
+        }
+        then = {
+          "@path" = "$.properties.orderId"
+        }
+      }
+    }
+    phone_number = {
+      "@if" = {
+        else = {
+          "@path" = "$.context.traits.phone"
+        }
+        exists = {
+          "@path" = "$.properties.phone"
+        }
+        then = {
+          "@path" = "$.properties.phone"
+        }
+      }
+    }
+    session_attributes_encoded = {
+      "@path" = "$.integrations.Google Ads Conversions.session_attributes_encoded"
+    }
+    session_attributes_key_value_pairs = {
+      gad_campaignid = {
+        "@path" = "$.properties.gad_campaignid"
+      }
+      gad_source = {
+        "@path" = "$.properties.gad_source"
+      }
+      landing_page_referrer = {
+        "@path" = "$.context.page.referrer"
+      }
+      landing_page_url = {
+        "@path" = "$.context.page.url"
+      }
+      landing_page_user_agent = {
+        "@path" = "$.context.userAgent"
+      }
+      session_start_time_usec = {
+        "@path" = "$.properties.session_start_time_usec"
+      }
+    }
+    user_ip_address = {
+      "@path" = "$.context.ip"
+    }
+    value = {
+      "@path" = "$.properties.total"
+    }
+  })
+  trigger = "event = \"Signup Form Completed\""
+}
