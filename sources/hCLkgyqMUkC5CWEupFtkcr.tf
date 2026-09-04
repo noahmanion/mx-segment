1a01464e0638c653de2ea013298f635893d8e0d4
Segment Git Sync <friends@segment.com>
2026-09-04 16:12:47 +0000

    Source 'hCLkgyqMUkC5CWEupFtkcr' created by noah@softpath.co

---
+++ sources/hCLkgyqMUkC5CWEupFtkcr.tf
@@ -0,0 +1,15 @@
+import {
+  to = segment_source.id-hCLkgyqMUkC5CWEupFtkcr
+  id = "hCLkgyqMUkC5CWEupFtkcr"
+}
+
+resource "segment_source" "id-hCLkgyqMUkC5CWEupFtkcr" {
+  enabled = false
+  labels  = null
+  metadata = {
+    id = "ODf0vA6dcH"
+  }
+  name     = "Facebook Lead Ads"
+  settings = jsonencode({})
+  slug     = "facebook_lead_ads"
+}
