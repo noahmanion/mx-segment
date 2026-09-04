f89beb76ff3eb3fcb23270350895022927d3319d
Segment Git Sync <friends@segment.com>
2026-09-04 16:21:52 +0000

    Destination Subscription '4U8zbHa9FfoNta5qbifBy2' created by noah@softpath.co

---
+++ destination_subscriptions/6a9af012190e658f8cee1814_4U8zbHa9FfoNta5qbifBy2.tf
@@ -0,0 +1,22 @@
+import {
+  to = segment_destination_subscription.id-6a9af012190e658f8cee1814_4U8zbHa9FfoNta5qbifBy2
+  id = "6a9af012190e658f8cee1814:4U8zbHa9FfoNta5qbifBy2"
+}
+
+resource "segment_destination_subscription" "id-6a9af012190e658f8cee1814_4U8zbHa9FfoNta5qbifBy2" {
+  action_id            = "uVzPR9SSpfLqF3zoPok99Q"
+  destination_id       = "6a9af012190e658f8cee1814"
+  enabled              = true
+  model_id             = null
+  name                 = "Create or Update an Organization"
+  reverse_etl_schedule = null
+  settings = jsonencode({
+    match_value = {
+      "@path" = "$.groupId"
+    }
+    name = {
+      "@path" = "$.traits.name"
+    }
+  })
+  trigger = "type = \"group\""
+}
