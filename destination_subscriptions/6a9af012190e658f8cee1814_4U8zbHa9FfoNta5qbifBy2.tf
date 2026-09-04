db663f84832367129ea9664198b5bcbbe0e5ba1e
Segment Git Sync <friends@segment.com>
2026-09-04 16:22:04 +0000

    Destination Subscription '4U8zbHa9FfoNta5qbifBy2' modified by noah@softpath.co

--- destination_subscriptions/6a9af012190e658f8cee1814_4U8zbHa9FfoNta5qbifBy2.tf
+++ destination_subscriptions/6a9af012190e658f8cee1814_4U8zbHa9FfoNta5qbifBy2.tf
@@ -6,7 +6,7 @@ import {
 resource "segment_destination_subscription" "id-6a9af012190e658f8cee1814_4U8zbHa9FfoNta5qbifBy2" {
   action_id            = "uVzPR9SSpfLqF3zoPok99Q"
   destination_id       = "6a9af012190e658f8cee1814"
-  enabled              = true
+  enabled              = false
   model_id             = null
   name                 = "Create or Update an Organization"
   reverse_etl_schedule = null
