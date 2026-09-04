664aec760aa82426423c123b783b52e79078e144
Segment Git Sync <friends@segment.com>
2026-09-04 16:13:30 +0000

    Source 's6wcFS6qdXWq8dt1U66TZe' created by noah@softpath.co

---
+++ sources/s6wcFS6qdXWq8dt1U66TZe.tf
@@ -0,0 +1,15 @@
+import {
+  to = segment_source.id-s6wcFS6qdXWq8dt1U66TZe
+  id = "s6wcFS6qdXWq8dt1U66TZe"
+}
+
+resource "segment_source" "id-s6wcFS6qdXWq8dt1U66TZe" {
+  enabled = false
+  labels  = null
+  metadata = {
+    id = "ODf0vA6dcH"
+  }
+  name     = "Facebook Lead Ads 2"
+  settings = jsonencode({})
+  slug     = "facebook_lead_ads_2"
+}
