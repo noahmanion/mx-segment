0981c30f1ae94a9612c1a21368ce4b6c80827b1f
Segment Git Sync <friends@segment.com>
2026-09-04 16:13:35 +0000

    Source 's6wcFS6qdXWq8dt1U66TZe' modified by noah@softpath.co

--- sources/s6wcFS6qdXWq8dt1U66TZe.tf
+++ sources/s6wcFS6qdXWq8dt1U66TZe.tf
@@ -9,7 +9,11 @@ resource "segment_source" "id-s6wcFS6qdXWq8dt1U66TZe" {
   metadata = {
     id = "ODf0vA6dcH"
   }
-  name     = "Facebook Lead Ads 2"
-  settings = jsonencode({})
-  slug     = "facebook_lead_ads_2"
+  name = "Facebook Lead Ads 2"
+  settings = jsonencode({
+    createdAt   = "2026-09-04T16:13:34.200Z"
+    createdBy   = "noah@softpath.co"
+    displayName = "Noah Manion"
+  })
+  slug = "facebook_lead_ads_2"
 }
