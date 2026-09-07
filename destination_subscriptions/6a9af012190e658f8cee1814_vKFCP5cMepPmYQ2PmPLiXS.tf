433f13a9e1b2d56608582c94c45365070cb4d2be
Segment Git Sync <friends@segment.com>
2026-09-07 19:53:48 +0000

    Destination Subscription 'vKFCP5cMepPmYQ2PmPLiXS' modified by noah@softpath.co

--- destination_subscriptions/6a9af012190e658f8cee1814_vKFCP5cMepPmYQ2PmPLiXS.tf
+++ destination_subscriptions/6a9af012190e658f8cee1814_vKFCP5cMepPmYQ2PmPLiXS.tf
@@ -15,6 +15,15 @@ resource "segment_destination_subscription" "id-6a9af012190e658f8cee1814_vKFCP5c
       "26f716e0c9f3d6cc0cee7bd51e2da4018bfe9bdd" = {
         "@path" = "$.traits.campaignName"
       }
+      "5cf8b6026b685e0b760999c9295f6505ed2f5cc2" = {
+        "@path" = "$.traits.adSetName"
+      }
+      b71a9d4d54446c84af8ad0e667e5c7dd9de123b0 = {
+        "@template" = "{{traits.what's_your_trade}}"
+      }
+      c1eacba1ed67ffb2a94106f085355a4f764785db = {
+        "@path" = "$.traits.adName"
+      }
     }
     email = {
       "@path" = "$.traits.email"
